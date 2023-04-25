336227d32c95423e377ab97cc32890468979392d
Zuzu work <muratov01@mail.ru>
2023-04-25 17:26:00 +0600

    Исправленное задание 6

---
+++ main.js
@@ -0,0 +1,78 @@
+   // домашка
+
+
+                //1 задание
+
+// let arg = +prompt ('Введите число')      
+// switch(arg){
+//     case 'да':                           
+//         alert('верю')                 
+//     case 'нет':                            
+//         alert('не верю 2')             
+       
+//     default:
+//         alert('сообщение не сработало')       
+// }
+// console.log( arg)
+
+             
+
+
+            //2 задание
+     
+     
+            // введите число от 1 до 12
+
+     
+// //let a = 12
+// switch (a){
+//     case 2:
+//         console.log('Зима');
+       
+//     case 5:
+//         console.log('Весна');      //точку
+       
+//     case 6:
+//         console.log('Лето');
+        
+//     default:
+//         console.log('Осень');
+// }
+
+
+  
+
+
+// второй вариант 
+
+
+let arg = +prompt ('Введите число от 1 до 12')    
+switch (arg){
+   case 1:
+   case 2:
+   case 3:
+        alert('Зима');
+        break
+   case 4:
+   case 5:
+   case 6:
+        alert('Весна');      
+        break
+   case 7:
+   case 8:
+   case 9:
+        alert('Лето');
+        break
+   default:
+        alert('Осень');
+}
+
+
+            //3 задание
+
+
+// let one = 'A'
+// let two = null
+// let tree = 'Z'
+// alert(one ?? two ?? tree ?? "Аноним")     // A
+
